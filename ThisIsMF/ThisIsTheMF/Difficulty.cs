698faed14583a39d5f24664d07fb7e8bb99a546e
ThursdayWitness <lirkir1256@gmail.com>
2021-05-11 17:22:58 +0500

    Закинул класс сложности, прошлый коммит это почему-то проигнорил

---
+++ ThisIsMF/ThisIsTheMF/Difficulty.cs
@@ -0,0 +1,9 @@
+﻿namespace ThisIsTheMF
+{
+    public class Difficulty
+    {
+        // Сложность игры. Пока вообще хрен его проймёт,
+        // что и как с этим делать, но чую, вещь важная.
+        public int level;
+    }
+}
