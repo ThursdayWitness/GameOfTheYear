69c55346cd7e5b21c1844016e1ece684c41ca296
ThursdayWitness <lirkir1256@gmail.com>
2021-05-29 02:00:13 +0500

    Удалил Day.cs

--- ThisIsMF/ThisIsTheMF/Day.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ThisIsTheMF
-{
-    public class Day // Игровой день. От дня зависят миссии
-    {
-        public int Difficulty; // От этого зависит кол-во миссий и их примерная сложность
-    }
-}
