f0441c57b512f19d9674c939c2e330e57aad5392
ThursdayWitness <lirkir1256@gmail.com>
2021-06-12 01:16:54 +0500

    Сложность готова для имплементации

--- ThisIsMF/ThisIsTheMF/Difficulty.cs
+++ ThisIsMF/ThisIsTheMF/Difficulty.cs
@@ -1,9 +1,29 @@
-﻿namespace ThisIsTheMF
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace ThisIsTheMF
 {
     public static class Difficulty
     {
-        // Сложность игры. Пока вообще хрен его проймёт,
-        // что и как с этим делать, но чую, вещь важная.
-        public static int DifficultyLevel;
+        public static int Level { get; private set; }
+        private static readonly Dictionary<int, string> Levels = new()
+        {
+            [1] = "Легко",
+            [2] = "Ничё так",
+            [3] = "Сложно"
+        };
+        
+        public static void SetLevel(string text)
+        {
+            foreach (var (key, value) in Levels)
+            {
+                if (value == text) Level = key;
+            }
+            //Level = Levels.Where(t => t.Key == text).Take(1).Select(t => t.Value);
+            //Это было бы потрясно сделать с помощью LINQ, но пока не могу это сделать.
+        }
+        
     }
 }
