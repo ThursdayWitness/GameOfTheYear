b1dc07cfd8d06686c144f24748864795d4a1c195
ThursdayWitness <lirkir1256@gmail.com>
2021-06-26 03:37:39 +0500

    Я ДОДЕЛАЛ ГЕНЕРАЦИЮ СОБЫТИЙ И ТЕПЕРЬ МОЖНО ХОТЯ БЫ ИГРАТЬ ВОТ ЭТО ДАААААААААААААААААААААААААА

--- ThisIsMF/ThisIsTheMF/Situation.Designer.cs
+++ ThisIsMF/ThisIsTheMF/Situation.Designer.cs
@@ -29,32 +29,104 @@ namespace ThisIsTheMF
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.descriptionText = new System.Windows.Forms.Label();
+            this.MainStudentBox = new System.Windows.Forms.ComboBox();
+            this.MainStudentLabel = new System.Windows.Forms.Label();
+            this.HelperLabel = new System.Windows.Forms.Label();
+            this.HelperStudent = new System.Windows.Forms.ComboBox();
+            this.SendTroops = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // label1
+            // descriptionText
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 48.22641F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(293, 137);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(97, 94);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "hi";
+            this.descriptionText.AutoSize = true;
+            this.descriptionText.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.descriptionText.Location = new System.Drawing.Point(12, 9);
+            this.descriptionText.MaximumSize = new System.Drawing.Size(500, 0);
+            this.descriptionText.Name = "descriptionText";
+            this.descriptionText.Size = new System.Drawing.Size(46, 45);
+            this.descriptionText.TabIndex = 0;
+            this.descriptionText.Text = "hi";
+            // 
+            // MainStudentBox
+            // 
+            this.MainStudentBox.FormattingEnabled = true;
+            this.MainStudentBox.Location = new System.Drawing.Point(12, 57);
+            this.MainStudentBox.Name = "MainStudentBox";
+            this.MainStudentBox.Size = new System.Drawing.Size(121, 21);
+            this.MainStudentBox.TabIndex = 1;
+            this.MainStudentBox.SelectionChangeCommitted += new System.EventHandler(this.MainStudentSelected);
+            // 
+            // MainStudentLabel
+            // 
+            this.MainStudentLabel.Location = new System.Drawing.Point(12, 92);
+            this.MainStudentLabel.Name = "MainStudentLabel";
+            this.MainStudentLabel.Size = new System.Drawing.Size(100, 23);
+            this.MainStudentLabel.TabIndex = 2;
+            this.MainStudentLabel.Text = "label1";
+            // 
+            // HelperLabel
+            // 
+            this.HelperLabel.Location = new System.Drawing.Point(12, 115);
+            this.HelperLabel.Name = "HelperLabel";
+            this.HelperLabel.Size = new System.Drawing.Size(100, 23);
+            this.HelperLabel.TabIndex = 3;
+            this.HelperLabel.Text = "label1";
+            this.HelperLabel.Visible = false;
+            // 
+            // HelperStudent
+            // 
+            this.HelperStudent.FormattingEnabled = true;
+            this.HelperStudent.Location = new System.Drawing.Point(139, 57);
+            this.HelperStudent.Name = "HelperStudent";
+            this.HelperStudent.Size = new System.Drawing.Size(121, 21);
+            this.HelperStudent.TabIndex = 4;
+            this.HelperStudent.Visible = false;
+            // 
+            // SendTroops
+            // 
+            this.SendTroops.AutoSize = true;
+            this.SendTroops.Location = new System.Drawing.Point(291, 136);
+            this.SendTroops.Name = "SendTroops";
+            this.SendTroops.Size = new System.Drawing.Size(120, 23);
+            this.SendTroops.TabIndex = 5;
+            this.SendTroops.Text = "Отправить бедолагу";
+            this.SendTroops.UseVisualStyleBackColor = true;
+            this.SendTroops.Click += new System.EventHandler(this.SendTroops_Click);
             // 
             // Situation
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(684, 378);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(586, 289);
+            this.Controls.Add(this.SendTroops);
+            this.Controls.Add(this.HelperStudent);
+            this.Controls.Add(this.HelperLabel);
+            this.Controls.Add(this.MainStudentLabel);
+            this.Controls.Add(this.MainStudentBox);
+            this.Controls.Add(this.descriptionText);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Situation";
             this.Text = "Situation";
             this.ResumeLayout(false);
             this.PerformLayout();
-
         }
+
+        private System.Windows.Forms.Button SendTroops;
+
+        private System.Windows.Forms.Label MainStudentLabel;
+
+        private System.Windows.Forms.Label HelperLabel;
+        private System.Windows.Forms.ComboBox HelperStudent;
+
+        private System.Windows.Forms.ComboBox MainStudentBox;
+
+        private System.Windows.Forms.ComboBox comboBox1;
+
+        private System.Windows.Forms.Label descriptionText;
+
+        private System.Windows.Forms.Label anchorLabel;
 
         #endregion
 
--- ThisIsMF/ThisIsTheMF/Situation.cs
+++ ThisIsMF/ThisIsTheMF/Situation.cs
@@ -12,13 +12,12 @@ namespace ThisIsTheMF
 {
     public partial class Situation : Form
     {
-        private static List<string> _locations = new()
+        private static readonly List<string> Locations = new()
         {
             "Матфаке", "СоцГуме", "ФЭИ", "СОКе"
         };
 
-        public static string Location = _locations[new Random().Next(0, _locations.Count - 1)]; 
-        private Tuple<int, int> requiredStat { get; } //Значение статы, Код статы
+        private Tuple<int, int> RequiredStat { get; } //Значение статы, Код статы
         //Коды стат:
         //0 - инта, 1 - красноречие, 2 - дерзость, 3 - сочувствие
         //Крч, идея такова:
@@ -27,22 +26,60 @@ namespace ThisIsTheMF
         //Проверяем это, в зависимости от результата происходит что-либо
         //Фишки статов расписал в Map.cs
 
-        public string Description;
-        private static readonly Dictionary<int, string> MissionDescriptions = new()
-        {
-            //Описания - своего рода подсказки к требуемой стате
-            [0] = $"Кто-нибудь понял последнюю тему и может её пояснить? Я жду в {Location}.",
-            [1] = $"Не могу сдать лабу, преподаватель отказывается меня слушать! Есть кто в {Location}?",
-            [2] = $"Дед снова за своё, так мы никогда не получим баллы! Нужно подкрепление в {Location}."
-        };
-
         public Situation()
         {
             InitializeComponent();
-            requiredStat = new Tuple<int, int>(new Random().Next(0, 8), new Random().Next(0,2));
-            Description = $"Хэй хэй, {Player.Name}! Нужна помощь! "+MissionDescriptions[requiredStat.Item2];
+            var missionLocation = Locations[new Random().Next(0, Locations.Count)];
+            RequiredStat = new Tuple<int, int>(new Random().Next(0, 8), new Random().Next(0,2));
+
+            var missionDescriptions = new Dictionary<int, string>
+            {
+                //Описания - своего рода подсказки к требуемой стате
+                [0] = $"Кто-нибудь понял последнюю тему и может её пояснить? Я жду в {missionLocation}.",
+                [1] = $"Не могу сдать лабу, преподаватель отказывается меня слушать! Есть кто в {missionLocation}?",
+                [2] = $"Дед снова за своё, так мы никогда не получим баллы! Нужно подкрепление в {missionLocation}."
+            };
+            
+            var description = $"Хэй хэй, {Player.Name}! Нужна помощь! " + missionDescriptions[RequiredStat.Item2];
+            descriptionText.Text = description;
+            foreach (var student in Player.StudentsList)
+            {
+                MainStudentBox.Items.Add(student.Name);
+            }
+
+            MainStudentLabel.Text = "Основной студент";
+            MainStudentLabel.Location = new Point(MainStudentLabel.Width/2,318-MainStudentLabel.Height*2);
+            MainStudentBox.Location = new Point(MainStudentLabel.Width/2,MainStudentLabel.Bottom);
+            
+            //Насрать на помощников, долго, нудно, забагованно. Мб потом.
+            //HelperLabel.Text = "Студент-помощник";
+            //HelperLabel.Location = new Point(MainStudentBox.Right, MainStudentLabel.Bottom);
+            //HelperStudent.Location = new Point(HelperLabel.Size.Width / 2, HelperLabel.Bottom);
+            ShowDialog();
         }
-        
-        
+
+        private void MainStudentSelected(object sender, EventArgs e)
+        {
+            //Метод нахрен не нужен, но удалив его придётся разбираться с ошибками,
+            //это недолго, но мне леееееень
+            //_availableHelpers.Remove((Student)MainStudentBox.SelectedItem);
+        }
+
+        private void SendTroops_Click(object sender, EventArgs e)
+        {
+            var name = MainStudentBox.Text;
+            if (Player.GetStudent(name).Stats[RequiredStat.Item2] >= RequiredStat.Item1)
+            {
+                MessageBox.Show("Красава, респект тебе и твоим работягам. Ещё сочтёмся!", "От души душевно в душу");
+                Player.TakeReputation();
+            }
+            else
+            {
+                MessageBox.Show("Не, ну у меня бы в одиночку лучше вышло, теперь ещё больше проблем");
+                Player.TakeDamage();
+                Map.UpdateLabel();
+            }
+            Close();
+        }
     }
 }
