e68a4e418c1e73805504bd715ab3c19f8b4a33da
ThursdayWitness <lirkir1256@gmail.com>
2021-06-25 15:10:21 +0500

    Refactor

--- ThisIsMF/ThisIsTheMF/RecruitsWindow.cs
+++ ThisIsMF/ThisIsTheMF/RecruitsWindow.cs
@@ -7,25 +7,22 @@ namespace ThisIsTheMF
 {
     public partial class RecruitsWindow : Form
     {
-        List<Student> studentsList = new();
+        public readonly List<Student> StudentsList = new();
         private Student _currentStudent;
         public RecruitsWindow()
         {
-            //Меню выбора рекрутируемых студентов
             InitializeComponent();
             GenerateStudent();
         }
         
         private void recruitStudent_Click(object sender, EventArgs e)
         {
-            studentsList.Add(_currentStudent);
+            StudentsList.Add(_currentStudent);
             Student._studentNames.Remove(_currentStudent.Name);
             recruitsLimit.Text = (int.Parse(recruitsLimit.Text)-1).ToString();
             if (int.Parse(recruitsLimit.Text) == 0)
             {
-                //studentsList.Remove(studentsList.Last());
-                //Из-за какого-то косяка в коде, добавляется лишний студент, как будет время - исправим адекватно. Пока так.
-                Player.StudentsList = studentsList;
+                Player.StudentsList = StudentsList;
                 new Map().ShowDialog();
                 Close();
             }
@@ -39,7 +36,6 @@ namespace ThisIsTheMF
 
         private void GenerateStudent()
         {
-            //if (studentsList.Count != 0) studentsList.Remove(studentsList.Last());
             _currentStudent = new Student();
             RecruitBox.Text = _currentStudent.Name;
             knowledgeStat.Text = _currentStudent.Stats[0].ToString();
